c3df590c7cbc6be9b5b1646f12cc10e8d754dde0
nrogers <nick@nickroge.rs>
2015-09-03 22:29:19 -0400

    Add wordList.json

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
+var wordList = [];
+jQuery(getWordList);
+
+function nextWord(count) {
+  var ret = [];
+  var i = 0;
+  for (; i < count; i++) {
+    var category = Math.floor(Math.random() * wordList.length)
+    var item = Math.floor(Math.random() * wordList[category].words.length)
+	ret.push({
+		'category': wordList[category].name,
+		'word': wordList[category].words[item]
+	});
+  }
+  return ret;
+}
+
+function getWordList() {
+  jQuery.getJSON('js/wordList.json', null, getWordListDone)
+}
+
+function getWordListDone(data,textStatus,jqXHR) {
+  wordList = data;
+}
+
 function jiggle(){
+  var word = nextWord(1);
+  jQuery('#word h1').html(word[0].word);
+  
   var bounce = new Bounce();
   bounce.translate({
     from: { x: -2200, y: 0 },
@@ -19,6 +47,6 @@ function jiggle(){
 }
 
 $$('#word').swipe(jiggle);
-$('#word').on('animationend', function(){
-	$('style').remove(); // remove the style tag this creates
+jQuery('#word').on('animationend', function(){
+  jQuery('style').remove(); // remove the style tag this creates
 });
